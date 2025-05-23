88f9def187ebcfc82f35ac571269b4e18a0a49d4
zoombots <33934673+zoombots@users.noreply.github.com>
2025-05-22 21:17:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 
 st.title("Covered Call Analysis")
 st.subheader("Risk & Reward Calculator")
-st.text_input("How to use calculator:")
+st.text("How to use calculator:")
 
 # Inputs
 symbol = st.text_input("Enter Stock Symbol:", "TSLA")
